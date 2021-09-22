541cdb8a0388b3b1e4ca5bdf6bb62b107fab1192
Saurabh Maurya <iamheremaurya@gmail.com>
2021-09-22 21:08:39 +0530

    removed expo dependency, complete react native application

--- components/Header.js
+++ components/Header.js
@@ -27,5 +27,3 @@ const styles = StyleSheet.create({
 });
 
 export default Header
-
-
--- components/TodoItem.js
+++ components/TodoItem.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import {MaterialIcons} from 'react-native-vector-icons';
+import Icon from 'react-native-vector-icons/MaterialIcons'
 import {  StyleSheet, Text, TouchableOpacity, View  } from 'react-native';
 
 const TodoItem = ({ item, onPressHandler }) => {
     return (
         <View style={styles.item}>
             <TouchableOpacity onPress={() => onPressHandler( item.key)}>
-                <MaterialIcons name="delete" size={18} color="#333" />
+                <Icon name="delete" size={18} color="maroon" />
             </TouchableOpacity>
             <Text style={styles.textItem}>{ item.text }</Text>
         </View>  
@@ -17,7 +17,7 @@ const styles = StyleSheet.create({
     item: {
         padding: 16,
         marginTop: 16,
-        borderColor: '#bbb',
+        borderColor: 'maroon',
         borderStyle: 'dashed',
         borderWidth: 1,
         borderRadius: 10,
